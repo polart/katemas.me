e63f83ea36315dd59192103110b42436ae1c6779
Artem Polunin <artem.pun@gmail.com>
2025-01-12 17:59:05 +0100

    Add Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://katemas.me',
+    prefetch: {
+        prefetchAll: true
+    },
+
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+
+    adapter: vercel()
 });
